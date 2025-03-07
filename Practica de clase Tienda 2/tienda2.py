2ac93c3732a7850335d40ab4af2cd3cdfd760df4
Heyder-83 <heydercolegious@gmail.com>
2025-03-07 00:40:44 -0500

    advance activity 2
    Update and upgrade the code

---
+++ Practica de clase Tienda 2/tienda2.py
@@ -0,0 +1,104 @@
+"""
+Práctica de clase: Tienda1
+
+Este programa maneja el inventario de una tienda.
+
+Heyder Ivan Ramos Rodriguez
+<hiramosr@academia.usbbog.edu.co
+ heydercolegious@gmail.com>
+2025-02-28
+"""
+
+
+class Producto:
+    """Clase para manejar productos"""
+
+    def __init__(self, nombre, precio_unitario, cantidad,
+                 descripcion, clasificacion):
+        self.nombre = nombre
+        self.precio_unitario = precio_unitario
+        self.cantidad = cantidad
+        self.descripcion = descripcion
+        self.clasificacion = clasificacion
+
+
+def solicitar_float(mensaje):
+    while True:
+        try:
+            numero = float(input(mensaje))
+            if numero < 0:
+                print("Por favor ingrese un precio valido (Mayor o igual a 0)")
+            else:
+                return numero
+        except ValueError:
+            print("Por favor ingrese un precio valido")
+
+
+def solicitar_int(mensaje):
+    while True:
+        try:
+            numero = int(input(mensaje))
+            if numero < 0:
+                print(
+                    "Por favor ingrese una cantidad valida (Mayor o igual a 0)"
+                )
+            else:
+                return numero
+        except ValueError:
+            print("Por favor ingrese una cantidad valida")
+
+
+def solicitar_descripcion(mensaje):
+    return input(mensaje)
+
+
+def solicitar_clasificacion(mensaje):
+    return input(mensaje)
+
+
+def mostrar_resumen(productos):
+    print("\n> Resumen:")
+    print("-" * 80)
+    print(
+        "| Producto  |  Cantidad     |   Precio     "
+        "|   Descripción     | Clasificación  |"
+    )
+    print("-" * 80)
+    for p_lista in productos:
+        print(f"> |{p_lista.nombre:<9}|{p_lista.cantidad:>4} unidades |"
+              f"{p_lista.precio_unitario:>7,.0f} pesos |"
+              f"{p_lista.descripcion:<20}|{p_lista.clasificacion:<12}")
+        # `:<9` alinea el texto a la izquierda con un ancho de 9 caracteres
+    print("-" * 80)
+
+
+def run():
+    productos = []  # Lista para almacenar los productos
+    numero_producto = 1
+
+    while True:
+        nombre = input(
+            f"Producto {numero_producto}, ¿cual es el nombre? "
+            f"(Dejar en blanco para terminar)\n"
+        )
+        if nombre == "":
+            break
+
+        precio_unitario = solicitar_float(f"¿Qué precio tiene {nombre}?\n")
+        cantidad = solicitar_int(f"¿Que cantidad hay de {nombre}?\n")
+        descripcion = solicitar_descripcion(f"Descripción breve de {nombre}\n")
+        clasificacion = solicitar_clasificacion(
+            f"¿En que clasificación se encuentra {nombre}?\n"
+        )
+        producto = Producto(
+            nombre, precio_unitario, cantidad, descripcion, clasificacion
+        )
+        productos.append(producto)  # Almacenar en la lista
+
+        numero_producto += 1
+
+    mostrar_resumen(productos)
+
+
+if __name__ == "__main__":
+    run()

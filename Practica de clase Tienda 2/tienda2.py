231d55ad4116dea14084afaac85a77f1b2fe26e0
Heyder-83 <heydercolegious@gmail.com>
2025-03-07 18:21:18 -0500

    Update

--- Practica de clase Tienda 2/tienda2.py
+++ Practica de clase Tienda 2/tienda2.py
@@ -14,12 +14,12 @@ class Producto:
     """Clase para manejar productos"""
 
     def __init__(self, nombre, precio_unitario, cantidad,
-                 descripcion, clasificacion):
+                 descripcion, categoria):
         self.nombre = nombre
         self.precio_unitario = precio_unitario
         self.cantidad = cantidad
         self.descripcion = descripcion
-        self.clasificacion = clasificacion
+        self.categoria = categoria
 
 
 def solicitar_float(mensaje):
@@ -52,7 +52,7 @@ def solicitar_descripcion(mensaje):
     return input(mensaje)
 
 
-def solicitar_clasificacion(mensaje):
+def solicitar_categoria(mensaje):
     return input(mensaje)
 
 
@@ -67,9 +67,17 @@ def mostrar_resumen(productos):
     for p_lista in productos:
         print(f"> |{p_lista.nombre:<9}|{p_lista.cantidad:>4} unidades |"
               f"{p_lista.precio_unitario:>7,.0f} pesos |"
-              f"{p_lista.descripcion:<20}|{p_lista.clasificacion:<12}")
+              f"{p_lista.descripcion:<20}|{p_lista.categoria:<12}")
         # `:<9` alinea el texto a la izquierda con un ancho de 9 caracteres
     print("-" * 80)
+
+
+def precio_categoria(productos):
+    if not productos:
+        print("\n No hay productos en la lista")
+        return
+    
+    clasificacion_precios = {}
 
 
 def run():
@@ -87,17 +95,18 @@ def run():
         precio_unitario = solicitar_float(f"¿Qué precio tiene {nombre}?\n")
         cantidad = solicitar_int(f"¿Que cantidad hay de {nombre}?\n")
         descripcion = solicitar_descripcion(f"Descripción breve de {nombre}\n")
-        clasificacion = solicitar_clasificacion(
+        categoria = solicitar_categoria(
             f"¿En que clasificación se encuentra {nombre}?\n"
         )
         producto = Producto(
-            nombre, precio_unitario, cantidad, descripcion, clasificacion
+            nombre, precio_unitario, cantidad, descripcion, categoria
         )
         productos.append(producto)  # Almacenar en la lista
 
         numero_producto += 1
 
     mostrar_resumen(productos)
+    precio_categoria(productos)
 
 
 if __name__ == "__main__":

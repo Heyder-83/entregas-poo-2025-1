3f1203f47e0cb300b9983c6df438a57575ccc86d
Heyder-83 <heydercolegious@gmail.com>
2025-03-07 22:33:24 -0500

    Final code

--- Practica de clase Tienda 2/tienda2.py
+++ Practica de clase Tienda 2/tienda2.py
@@ -74,10 +74,25 @@ def mostrar_resumen(productos):
 
 def precio_categoria(productos):
     if not productos:
-        print("\n No hay productos en la lista")
+        print("\nNo hay productos en la lista")
         return
     
-    clasificacion_precios = {}
+    precios_categoria = {}
+
+    for product in productos:
+        total_precio = product.precio_unitario * product.cantidad
+        if product.categoria in precios_categoria:
+            precios_categoria[product.categoria] += total_precio
+        else:
+            precios_categoria[product.categoria] = total_precio
+
+    print("\n> Precios por clasificación:")
+    print("-" * 40)
+    print(f"| {'Clasificación':<15} | {'Precio':<10} |")
+    print("-" * 40)
+    for categoria, precio in precios_categoria.items():
+        print(f"| {categoria:<15} | {precio:>7,.0f} pesos |")
+    print("-" * 40)
 
 
 def run():
@@ -111,3 +126,79 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+"""
+Producto 1, ¿cual es el nombre? (Dejar en blanco para terminar)
+Leche
+¿Qué precio tiene Leche?
+4800
+¿Que cantidad hay de Leche?
+33
+Descripción breve de Leche
+Bolsa 900 ml
+¿En que clasificación se encuentra Leche?
+Lacteo
+Producto 2, ¿cual es el nombre? (Dejar en blanco para terminar)
+Yogourt
+¿Qué precio tiene Yogourt?
+3200
+¿Que cantidad hay de Yogourt?
+45 
+Descripción breve de Yogourt
+Bolsa 600 ml
+¿En que clasificación se encuentra Yogourt?
+Lacteo
+Producto 3, ¿cual es el nombre? (Dejar en blanco para terminar)
+Arroz
+¿Qué precio tiene Arroz?
+2200
+¿Que cantidad hay de Arroz?
+43
+Descripción breve de Arroz
+Libra de arroz
+¿En que clasificación se encuentra Arroz?
+Cereal
+Producto 4, ¿cual es el nombre? (Dejar en blanco para terminar)
+Jabon rey
+¿Qué precio tiene Jabon rey?
+2200
+¿Que cantidad hay de Jabon rey?
+24
+Descripción breve de Jabon rey
+Jabon barra
+¿En que clasificación se encuentra Jabon rey?
+Aseo
+Producto 5, ¿cual es el nombre? (Dejar en blanco para terminar)
+Cloro
+¿Qué precio tiene Cloro?
+2900
+¿Que cantidad hay de Cloro?
+30
+Descripción breve de Cloro
+Botella 1000 ml
+¿En que clasificación se encuentra Cloro?
+Aseo
+Producto 6, ¿cual es el nombre? (Dejar en blanco para terminar)
+
+
+> Resumen:
+--------------------------------------------------------------------------------
+| Producto  |  Cantidad     |   Precio     |   Descripción     | Clasificación  |
+--------------------------------------------------------------------------------
+> |Leche    |  33 unidades |  4,800 pesos |Bolsa 900 ml        |Lacteo      
+> |Yogourt  |  45 unidades |  3,200 pesos |Bolsa 600 ml        |Lacteo      
+> |Arroz    |  43 unidades |  2,200 pesos |Libra de arroz      |Cereal      
+> |Jabon rey|  24 unidades |  2,200 pesos |Jabon barra         |Aseo        
+> |Cloro    |  30 unidades |  2,900 pesos |Botella 1000 ml     |Aseo        
+--------------------------------------------------------------------------------
+
+> Precios por clasificación:
+----------------------------------------
+| Clasificación   | Precio     |
+----------------------------------------
+| Lacteo          | 302,400 pesos |
+| Cereal          |  94,600 pesos |
+| Aseo            | 139,800 pesos |
+----------------------------------------
+
+"""

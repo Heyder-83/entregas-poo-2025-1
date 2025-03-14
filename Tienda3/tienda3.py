e6d3ffe7125b3b43ad869e8ad1f0f13e26939253
Heyder-83 <heydercolegious@gmail.com>
2025-03-14 12:58:45 -0500

    Modify names

---
+++ Tienda3/tienda3.py
@@ -0,0 +1,13 @@
+"""
+Práctica de clase: Tienda1
+
+Este programa maneja el inventario de una tienda.
+
+Heyder Ivan Ramos Rodriguez
+<hiramosr@academia.usbbog.edu.co
+ heydercolegious@gmail.com>
+2025-02-28
+"""
+
+
+class Producto:
